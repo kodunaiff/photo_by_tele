6c2162bbed33268cd703a4a983cd10eef360b808
kodunaiff <kodunaiff@gmail.com>
2023-10-04 00:02:35 +0800

    Changed fetch_nasa_earth.py
    1)  изменил порядок слов в переменных.
    2) добавил нумерацию индексирования с единицы без сложения.
    3) скорректировал названия переменных, дал более четкую формулировку.
    4) сократил количество сетевых запросов.

--- fetch_nasa_earth.py
+++ fetch_nasa_earth.py
@@ -1,30 +1,30 @@
 import os
 
 import requests
+from dotenv import load_dotenv
 
 from get_file_extension_image import save_image
 
 
 def main():
+    load_dotenv()
+    nasa_token = os.environ['TOKEN_NASA']
     params = {
-        'api_key': 'DEMO_KEY'
+        'api_key': nasa_token
     }
     url_ex = 'https://api.nasa.gov/EPIC/api/natural/images'
 
     response = requests.get(url_ex, params=params)
     response.raise_for_status()
-    epic_data = response.json()
-    for epic_number, epic_record in enumerate(epic_data):
-        date_image, time_image = epic_record['date'].split()
-        year, month, day = date_image.split('-')
-        name_image = epic_record['image']
-        save_path = os.path.join('images', f'earth_picture{epic_number + 1}.png')
+    epic_images = response.json()
+    for epic_number, epic_record in enumerate(epic_images, start=1):
+        image_date, image_time = epic_record['date'].split()
+        year, month, day = image_date.split('-')
+        image_name = epic_record['image']
+        save_path = os.path.join('images', f'earth_picture{epic_number}.png')
         url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}' \
-              f'/{day}/'f'png/{name_image}.png'
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        image_url = response.url
-        save_image(save_path, image_url)
+              f'/{day}/'f'png/{image_name}.png'
+        save_image(save_path, url, nasa_token)
 
 
 if __name__ == "__main__":
