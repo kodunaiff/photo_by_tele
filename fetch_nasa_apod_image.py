cbe1d430bd173f4a0392f4879503529f6978a73a
kodunaiff <kodunaiff@gmail.com>
2023-10-03 23:54:50 +0800

    Changed fetch_nasa_apod_image.py
    1)  изменил порядок слов в переменных
    2) добавил нумерацию индексирования с единицы без сложения.

--- fetch_nasa_apod_image.py
+++ fetch_nasa_apod_image.py
@@ -7,33 +7,33 @@ from dotenv import load_dotenv
 from get_file_extension_image import get_file_extension, save_image
 
 
-def fetch_nasa_apod(count_image, token):
+def fetch_nasa_apod(image_count, token):
     url = 'https://api.nasa.gov/planetary/apod'
 
     payload = {
-        'count': f'{count_image}',
+        'count': f'{image_count}',
         'api_key': f'{token}',
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
 
     nasa_pictures = response.json()
-    for link_number, link in enumerate(nasa_pictures):
+    for link_number, link in enumerate(nasa_pictures, start=1):
         form = get_file_extension(link['url'])
-        save_path = os.path.join('images', f'nasa_apod{link_number + 1}{form}')
+        save_path = os.path.join('images', f'nasa_apod{link_number}{form}')
         save_image(save_path, link['url'])
 
 
 def main():
     load_dotenv()
-    token = os.getenv('TOKEN_NASA')
+    nasa_token = os.getenv('TOKEN_NASA')
     parser = argparse.ArgumentParser(
         description='Скачивает популярные изображения космоса'
     )
-    parser.add_argument('count_image', help='give me count')
+    parser.add_argument('image_count', help='give me count')
     args = parser.parse_args()
 
-    fetch_nasa_apod(args.count_image, token)
+    fetch_nasa_apod(args.image_count, nasa_token)
 
 
 if __name__ == "__main__":
